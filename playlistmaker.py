50db738351dc06f37e9b5ae6b23cb35c0c84530d
Fausto Tommasi <mmasito123@yahoo.com>
2017-06-11 15:58:04 -0500

    added example application

--- playlistmaker.py
+++ playlistmaker.py
@@ -9,11 +9,11 @@ playlistFile = open(filename,"r")
 for line in playlistFile:
   line_split = line.split()
   song = line_split[0]
-  link = line_split[1]
+  name = line_split[1]
 
 
-  print "starting download for", song
-  song_process = subprocess.Popen("./Youtubeclipper f " + song + " " + name + ".mp3")
+  print "starting download for", name, song
+  song_process = subprocess.Popen(("./YoutubeToClip.py f " + song + " " + name + ".mp3").split())
   song_process.wait()
   print "download for", song, "complete"
 
